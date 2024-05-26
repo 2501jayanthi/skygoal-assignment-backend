7578b7a7acbf35f4edbb77fe2ab28e8588ccab45
2501jayanthi <2501jayanthi@gmail.com>
2024-05-26 15:13:16 +0530

    updated

--- index.js
+++ index.js
@@ -58,6 +58,33 @@ const initializeDBAndServer = async () => {
     response.send(user);
   });
 
+  //Register User
+  app.post("/users/", async (request, response) => {
+    const { username,  password, gender, location } = request.body;
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+    const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
+    const dbUser = await db.get(selectUserQuery);
+    if (dbUser === undefined) {
+      const createUserQuery = `
+        INSERT INTO 
+          user (username, name, password, gender, location) 
+        VALUES 
+          (
+            '${username}', 
+            '${hashedPassword}', 
+            '${gender}',
+            '${location}'
+          )`;
+      const dbResponse = await db.run(createUserQuery);
+      const newUserId = dbResponse.lastID;
+      response.send(`Created new user with ${newUserId}`);
+    } else {
+      response.status = 400;
+      response.send("User already exists");
+    }
+  });
+
+
   // Delete User API
   
   app.delete("/users/:userId", async (request, response) => {
@@ -69,4 +96,28 @@ const initializeDBAndServer = async () => {
         id=${userId};`;
     await db.all(deleteuserQuery);
     response.send("User Deleted");
+  });
+
+  //Login User
+
+  app.post("/login", async (request, response) => {
+    const { username, password } = request.body;
+    const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+    const dbUser = await db.get(selectUserQuery);
+    if (dbUser === undefined) {
+      response.status(400);
+      response.send("Invalid User");
+    } else {
+      const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+      if (isPasswordMatched === true) {
+        const payload = {
+          username: username,
+        };
+        const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+        response.send({ jwtToken });
+      } else {
+        response.status(400);
+        response.send("Invalid Password");
+      }
+    }
   });
